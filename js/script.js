7686926f3bb0110c319b9c46b71860ec71617110
Bianca Danforth <biancadanforth@gmail.com>
2017-01-13 12:22:27 -0800

    Make new layout for header and first section
    Remove 'Under Construction' notification bar at top...
    Remove 'Design' and 'Development' icons in 'Work' section
    Remove 'Welcome' <h1> element
    Consolidate header, adding 'Bianca Danforth' next to the logo.
    Update welcome blurb to be more memorable. Including adding favorite tools and a call-to-action.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
-// TEMPORARY UNDER CONSTRUCTION NOTIFY BAR
 
-var rectangle = document.getElementById("rectangle");
-var closeLink = document.getElementById("close");
-
-setTimeout(showRectangle, 1500);
-
-closeLink.onclick = hideRectangle;
-
-function showRectangle() {
-  rectangle.classList.remove("hidden");
-}
-
-function hideRectangle() {
-  rectangle.classList.add("hidden");
-}
