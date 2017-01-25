44603f996cf95ad52ef00c859303d08014144a26
Bianca Danforth <biancadanforth@gmail.com>
2017-01-25 08:08:17 -0800

    Add fixed header with transitions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+var header = document.getElementById("header");
 
+window.addEventListener('scroll', minimizeHeader);
+
+function minimizeHeader() {
+	if (document.body.scrollTop === 0) {
+		header.classList.remove("minimize");
+	} else {
+		header.classList.add("minimize");
+	}
+}
+
+var h = header.clientHeight;
+var main = document.getElementById("main");
+main.style.paddingTop = h.toString() + "px";
