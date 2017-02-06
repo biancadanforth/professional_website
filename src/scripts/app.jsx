f146ad7d98cf7ed5ddb171d0d9f7a68ae8285b5c
Bianca Danforth <biancadanforth@gmail.com>
2017-02-06 11:30:28 -0800

    Add click events to Header component <nav> links

---
+++ src/scripts/app.jsx
@@ -0,0 +1,14 @@
+class App extends React.Component {
+  render() {
+    return (
+    	<Header />
+      // <Main />
+      // <Footer />
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
