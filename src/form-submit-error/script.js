d24670ef48002a8c679b9e0e9789e6c77f8adafc
Bianca Danforth <biancadanforth@gmail.com>
2017-02-17 14:35:36 -0800

    Add form-submit-error page

---
+++ src/form-submit-error/script.js
@@ -0,0 +1,21 @@
+// form-submit-error script
+
+if(window.addEventListener) {
+  window.addEventListener('load', startTimer);
+}
+
+function startTimer() {
+  let countdown = 14;
+  let interval = setInterval(function() {
+    document.getElementById("timer-readout").textContent = countdown.toString();
+    console.log(countdown.toString());
+    countdown--;
+    if (countdown <= 0) {
+      clearInterval(interval);
+    } 
+  }, 1000);
+
+  setTimeout(() => window.location = "/#/more-info"
+    , 15000);
+}
+
