78bb5cb500ce76b02e385a82e7b861e23c0250d9
Bianca Danforth <biancadanforth@gmail.com>
2017-04-15 21:37:49 -0700

    Add job title in <header>

--- src/scripts/components/Header.jsx
+++ src/scripts/components/Header.jsx
@@ -96,8 +96,11 @@ class Header extends React.Component {
             </Link>
             <Link
               to='home'
+              className="header-id-link"
               aria-label="Go to Bianca Danforth's website">
               <h1 className="header-name">Bianca Danforth</h1>
+              <h2 className="header-role">Web Designer & <span>Front-End </span>Developer</h2>
+              <h2 className="header-role mobile">Web Designer/Developer</h2>
             </Link>
           </span>
            <nav role="navigation">
