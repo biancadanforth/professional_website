c64ec228bfceaa1ca11cd332c2468faf3d73b402
Bianca Danforth <biancadanforth@gmail.com>
2020-09-07 10:05:46 -0700

    Partial update to Extension Storage project page copy.
    Only updated Overview and Gather Information sections satisfactorily thus far. Images and links in Process section still need updating.

--- src/scripts/components/ExtensionStorage.jsx
+++ src/scripts/components/ExtensionStorage.jsx
@@ -11,7 +11,7 @@ class ExtensionStorage extends React.Component {
           <div className='primary-wrapper'>
             <h1 className='project-header'>Extension Storage</h1>
             <h3 className='project-subsubheader'>
-              Front-End & Browser Development | Developer Tools
+              Browser Development | Developer Tools
             </h3>
             <div className='project-hero-wrapper'>
               <div className='glamour-shot-wrapper'>
@@ -44,29 +44,20 @@ class ExtensionStorage extends React.Component {
                   Project goals
                 </h4>
                 <p className='project-overview-description'>
-                  The goal of this project was to ship a novel e-commerce product in Firefox by Cyber Monday.
+                  The goal of this project was to add support in Firefox's Developer Tools to read and write to browser extension <a href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/local" target="_blank">local storage</a>.
                 </p>
                 <p className='project-overview-description'>
-                  <a href="https://www.youtube.com/watch?v=UpRLjTQmkW4" target="_blank">Price Wise</a>, later renamed Price Tracker, is a shopping assistant that helps users find the best time to buy products they are interested in.
+                  Prior to this work, there was no DevTools support for extension storage of any kind. This was a feature extension developers had been requesting for <a href="https://bugzilla.mozilla.org/show_bug.cgi?id=1292234" target="_blank">several years</a>.
                 </p>
                 <p className='project-overview-description'>
-                  We managed to ship the product two weeks early, and it was featured in several press articles at the time, including <a href="https://www.nytimes.com/2018/11/13/smarter-living/how-to-tell-if-black-friday-deals-are-worth-buying.html" target="_blank">The New York Times</a>, <a href="https://techcrunch.com/2018/11/12/firefoxs-newest-test-pilot-experiments-help-you-track-prices-and-email-links/" target="_blank">Tech Crunch</a> and <a href="https://venturebeat.com/2018/11/12/firefox-debuts-experimental-price-tracking-feature-for-consumers/" target="_blank">Venture Beat</a>.
+                  While this project initially started as part of my efforts to improve Firefox's experiments infrastructure, the vast majority of the work was completed as a side project in my spare time.
                 </p>
                 <h4 className='project-overview-subheader'>Tools and Methods</h4>
-                 <p className='project-overview-description'>
-                  Price Tracker is a <a href="https://wiki.mozilla.org/Test_Pilot" target="_blank">Test Pilot</a> browser extension with some extra privileges to enable us to record <a href="https://github.com/mozilla/price-tracker/blob/master/docs/METRICS.md" target="_blank">telemetry</a> using Firefox's data infrastructure.
+                <p className='project-overview-description'>
+                  This work was in Javascript, where unit, integration and functional tests used Mozilla's <a href="https://developer.mozilla.org/en-US/docs/Mozilla/QA/Writing_xpcshell-based_unit_tests" target="_blank">XPCShell</a> and <a href="https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Mochitest" target="_blank">Mochitest</a> frameworks.
                 </p>
                 <p className='project-overview-description'>
-                  The source files are written with React and Redux; they are transpiled and bundled using Webpack.
-                </p>
-                <p className='project-overview-description'>
-                  Product information is extracted from web content using a machine learning model developed with <a href="https://mozilla.github.io/fathom/intro.html#" target="_blank">Fathom</a>.
-                </p>
-                <p className='project-overview-description'>
-                  CircleCI was used to run tests and deploy new releases of the extension.
-                </p>
-                <p className='project-overview-description'>
-                I completed this work along with one other engineer and a small, cross-functional team.
+                I completed this work with weekly mentorship from an engineer on the WebExtensions team.
                 </p>
               </div>
             </div>
@@ -87,10 +78,10 @@ class ExtensionStorage extends React.Component {
                 </figure>
                 <div className='process-description'>
                   <p className='project-description'>
-                    The first two months were largely explorations in the existing e-commerce space including studies led by our user researcher.
+                    There was a research spike at the beginning as I had to orient myself with the DevTools code base, which has evolved separately from Firefox and has its own architecture and terminology.
                   </p>
                   <p className='project-description'>
-                    We also spent some time talking to potential external partners and building the scaffolding of the project in GitHub.
+                    Specifically, Firefox's Developer Tools has a client-server architecture where entities called "actors" on the server exchange JSON packets with the client using the <a href="https://firefox-source-docs.mozilla.org/devtools/backend/protocol.html" target="_blank">Remote Debugging Protocol</a>.
                   </p>
                 </div>
               </div>
@@ -105,13 +96,11 @@ class ExtensionStorage extends React.Component {
                 />
                 <div className='process-description'>
                   <p className='project-description'>
-                    To identify products on web pages, we initially used <a href="https://ogp.me/" target="_blank">Open Graph</a>, though we soon learned it is not widely adopted by e-commerce sites.
+                    I had to create a new Extension Storage actor that interfaced with the IndexedDB storage back-end used by the extension <code>storage.local</code> API and ensure that it was loaded and populated correctly in the client.
                   </p>
                   <p className='project-description'>
-                    Looking for alternatives, I spent two weeks prototyping a machine learning model.
                   </p>
                   <p className='project-description'>
-                    Given the options we had available, including leveraging an external partner's APIs, the team went with my recommendation to use the model.
                   </p>
                 </div>
               </div>
